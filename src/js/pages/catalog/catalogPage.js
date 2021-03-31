c570bfef54dbd3161b2724c67f070136381d4954
Eeveeee <fx8300gfe@gmail.com>
2021-03-31 18:16:36 +0300

    added fake bd, filtration by link

--- src/js/pages/catalog/catalogPage.js
+++ src/js/pages/catalog/catalogPage.js
@@ -1,61 +1,94 @@
-const bd = {
-  pc1: {
-    name: 'Work',
-    price: '',
-    info:
-      'CPU:AMD Ryzen Threadripper 3990X, GPU:RTX 3090 FEx2, RAM:512gb DDR4 G.Skill Trident, PCU: Seasonic 1200w Platinum, SSD: 2tb Samsung EVO 960x4, Motherboard: Asus Zenith sTRX4, Cooler:Cooler Master Wraith Ripper sTRX4, Case: Cooler Master Cosmos',
-    img: '../images/home/work-pc.webp',
-    type: 'work',
-  },
-  pc2: {
-    name: 'Gaming',
-    price: '',
-    info:
-      'CPU:AMD Ryzen 9 5950X, GPU:RTX 3080 FE, RAM:32gb DDR4 G.Skill Ripjaws, PCU: Super Flower 700w Platinum, SSD: 1tb Samsung EVO 960x2, Motherboard: Asrock Taichi X570, Cooler:Noctua NH-15, Case: Deepcool Matrex 70',
-    img: '../images/home/gaming-pc.webp',
-    type: 'gaming',
-  },
-  pc3: {
-    name: 'Office',
-    price: '',
-    info:
-      'CPU:AMD Ryzen 5 PRO 4650G, GPU: IGP,, RAM:8gb DDR4 Samsung, 300w FSP, HDD: 500GB WD Blue, Motherboard: Asrock B450, Cooler:BOX, Case:DEXP DC-202M',
-    img: '../images/home/office-pc.webp',
-    type: 'office',
-  },
-}
-function renderGoods(goods) {}
-function filter(type, by = '-') {}
-export function catalogPage(render, addListener, changeLink, catalogPageHTML) {
-  const bd = {
-    pc1: {
-      name: 'Work',
-      price: '',
-      info:
-        'CPU:AMD Ryzen Threadripper 3990X, GPU:RTX 3090 FEx2, RAM:512gb DDR4 G.Skill Trident, PCU: Seasonic 1200w Platinum, SSD: 2tb Samsung EVO 960x4, Motherboard: Asus Zenith sTRX4, Cooler:Cooler Master Wraith Ripper sTRX4, Case: Cooler Master Cosmos',
-      img: '../images/home/work-pc.webp',
-      type: 'work',
-    },
-    pc2: {
-      name: 'Gaming',
-      price: '',
-      info:
-        'CPU:AMD Ryzen 9 5950X, GPU:RTX 3080 FE, RAM:32gb DDR4 G.Skill Ripjaws, PCU: Super Flower 700w Platinum, SSD: 1tb Samsung EVO 960x2, Motherboard: Asrock Taichi X570, Cooler:Noctua NH-15, Case: Deepcool Matrex 70',
-      img: '../images/home/gaming-pc.webp',
-      type: 'gaming',
-    },
-    pc3: {
-      name: 'Office',
-      price: '',
-      info:
-        'CPU:AMD Ryzen 5 PRO 4650G, GPU: IGP,, RAM:8gb DDR4 Samsung, 300w FSP, HDD: 500GB WD Blue, Motherboard: Asrock B450, Cooler:BOX, Case:DEXP DC-202M',
-      img: '../images/home/office-pc.webp',
-      type: 'office',
-    },
-  }
-  Object.keys(bd).forEach((key) => {
-    console.log(key)
-  })
+const filterCheckboxes = document.querySelectorAll('.filter-checkbox')
+const sortBySelector = document.querySelector('.sort-by-selector')
 
-  render(catalogPageHTML)
+async function getData() {
+  const response = await fetch('http://localhost:1488/bd.json', {
+    method: 'GET',
+  })
+  if (!response.ok) {
+    return
+  }
+  const data = await response.json()
+  return data
+}
+
+function getCardHTML(img, title, info, price) {
+  return `
+  <div class="card">
+  <div class="card-photo-wrapper">
+  <img src="${img}" alt="" class="card-photo" />
+  </div><div class='card-info'>
+  <span class="card-title">${title}</span>
+  <span class="card-specs">${info}</span>
+  <span class="card-price"><span>Цена:</span> ${price} <span>Руб</span></span></div>
+  </div>
+  `
+}
+
+function renderCards(computersArr) {
+  const cardsContainer = document.querySelector('.cards-container')
+  cardsContainer.innerHTML = ''
+  computersArr.forEach((card) => {
+    cardsContainer.insertAdjacentHTML(
+      'beforeend',
+      getCardHTML(card.img, card.name, card.info, card.price)
+    )
+  })
+}
+function sortBy(by, cardsArrFiltered) {
+  let sortedArr = cardsArrFiltered
+  const cardsArr = []
+  switch (by) {
+    case 'DESC':
+      sortedArr.sort((a, b) => a.price - b.price)
+      break
+    case 'ASC':
+      sortedArr.sort((a, b) => b.price - a.price)
+
+      break
+
+    default:
+      break
+  }
+  console.log(sortedArr)
+  return cardsArrFiltered
+}
+function filter(types, by = 'DESC', computersArr) {
+  const cardsArr = computersArr
+  console.log(types)
+  let cardsArrFiltered = []
+  if (types) {
+    if (typeof types === 'object') {
+      console.log('obj!!')
+      computersArr.forEach((computer) => {
+        types.forEach((type) => {
+          if (computer.keywords.includes(type)) {
+            cardsArrFiltered.push(computer)
+          }
+        })
+      })
+    } else {
+      console.log('string!!!')
+      computersArr.forEach((computer) => {
+        if (computer.keywords.includes(types)) {
+          cardsArrFiltered.push(computer)
+        }
+      })
+    }
+  } else {
+    cardsArrFiltered = computersArr
+  }
+  cardsArrFiltered = sortBy(by, cardsArrFiltered)
+  console.log(cardsArrFiltered)
+  renderCards(cardsArrFiltered)
+}
+export function catalogPage(render, addListener, changeLink, catalogPageHTML) {
+  getData().then((res) => {
+    const computersArr = res
+    console.log(computersArr)
+    render(catalogPageHTML)
+    renderCards(computersArr)
+    let filterRout = window.location.href.split('#')[2]
+    filter(filterRout, 'DESC', computersArr)
+  })
 }
--- src/js/pages/catalog/catalogPageHTML.js
+++ src/js/pages/catalog/catalogPageHTML.js
@@ -2,11 +2,45 @@ const catalogPageHTML = `
 <div class="catalog">
 <div class="container">
   <div class="catalog-outer">
-    <div class="catalog-filter"></div>
-    <div class="goods-container">
-      <div class="test-pc"></div>
+    <div class="catalog-filter">
+      <select class="sort-by-selector">
+        <option data-sort="highest" class="sort-by-option">
+          По возрастанию
+        </option>
+        <option data-sort="lowest" class="sort-by-option">
+          По убыванию
+        </option>
+      </select>
+      <div class="filter-options">
+        <label class="filter-option">
+          <input
+            data-sort="office"
+            type="checkbox"
+            class="filter-checkbox"
+          />
+          <div class="filter-name">Офисный</div>
+        </label>
+        <label class="filter-option">
+          <input
+            data-sort="gaming"
+            type="checkbox"
+            class="filter-checkbox"
+          />
+          <div class="filter-name">Игровой</div>
+        </label>
+        <label class="filter-option">
+          <input
+            data-sort="work"
+            type="checkbox"
+            class="filter-checkbox"
+          />
+          <div class="filter-name">Рабочая станция</div>
+        </label>
+      </div>
     </div>
+    <div class="cards-container"></div>
   </div>
 </div>
-</div>`
+</div>
+`
 export default catalogPageHTML
